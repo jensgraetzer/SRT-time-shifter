60d270cb2fc1e7766cae35d1bdd73a6c22d6eb69
Jens Grätzer <44551846+jensgraetzer@users.noreply.github.com>
2023-06-02 20:54:28 +0200

    Added one more script: srtTimeCorrector .py

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # SRT-time-shifter
-This very simple Python script shifts all the times in a SRT subtitle file by a given time span.
 
-Let's say, you have subtitles, belonging to a mediafile. The subtitle file is in the SRT format. And you want to shift all subtitle times by a given time span. Than write the name of the subtitle file and the time span into the file srtTimeShifter.ini. Next, run the script srtTimeShifter.py. The script reads the subtitle file and outputs a new subtitle file. The whole process is shown in this Data Processing Map:
+This very simple Python scripts shift all the times in a SRT subtitle file.
+
+Say, you have a mediafile (a video or an audio file) with subtitles. The subtitles are stored in a separate subtitle file - a textfile in SRT format. Imagine, the time values in the subtitle file need to be shifted, to be accurate.
+
+Here are are two python scripts. The first script helps you to shift all time values by a constant time. The second script shifts times in a way, that they fit at the beginning and the end of the mediafile. This is useful in the case, that the the media file runs at a slightly different speed as expected by the creator of the subtitles file.
+
+## srtTimeShifter.py
+
+Do you want to shift all subtitle times by a constant time span? Than write the name of the subtitle file and the time span into the file srtTimeShifter.ini. 
+
+Next, run the script srtTimeShifter.py. The script reads the subtitle file and outputs a new subtitle file with the new times.
+
+The whole process is shown in this Data Processing Map:
 
 ![Data Processing Map](srtTimeShifter_dataProcessingMap.png)
 
@@ -10,3 +21,24 @@ Here is an example of the srtTimeShifter.ini file:
 inputFileName = "file.srt"
 
 addSeconds = -3.435
+
+## srtTimeCorrector.py
+
+Do you want to shift all subtitle times in a way, that they fit to a media file, that runs at a sligtly different speed? Than write the name of the subtitle file srtTimeCorrector.ini. Add a pair of times (old, new time) from the beginning of the media file. And add a pair of times (old, new time) from the beginning of the media file.
+
+Next, run the script srtTimeCorrector.py. The script reads the subtitle file and outputs a new subtitle file with the new times.
+
+The Data Processing Map of this process is similar to the Data Processing Map above.
+
+Here is an example of the srtTimeCorrector.ini file:
+
+inputFileName = test.srt
+
+time1oldInSRT = 00:01:15.700
+
+time1inVideo  = 00:01:20.100
+
+time2oldInSRT = 01:20:58.500
+
+time2inVideo  = 01:20:54.200
+
--- srtTimeCorrector.py
+++ srtTimeCorrector.py
@@ -1,39 +1,39 @@
 '''
-Change all times in a SRT file: Add some seconds
-Author: J. Grätzer, 2020-01-22
+Change all times in a SRT file to a video with slightly different speed
+Author: J. Grätzer, 2021-06-14
 
 Input: 2 files
 * Subtitle-file in SRT format, UTF-8
-* The file srtTimeShifter.ini
+* The file srtTimeCorrector.ini
   ---------------------------------
-  | inputFileName = filename.srt  |
-  | addSeconds = -25.7            |
+  | inputFileName = test.srt      |
+  | time1oldInSRT = 00:01:15.700  | ... time1 is near the start of the video
+  | time1inVideo  = 00:01:20.100  |
+  | time2oldInSRT = 01:20:58.500  | ... time2 is near the end of the video
+  | time2inVideo  = 01:20:54.200  |
   ---------------------------------
 
 Output is: filename.srt_new.srt
 
 '''
 import re
+from decimal import Decimal
 
 # --- SETTINGS ---
-iniFileName = "srtTimeShifter.ini"
+iniFileName = "srtTimeCorrector.ini"
 
 # --- FUNCTIONS ---
 def hhmmsssssLineToSecondsFloat(line):
-    ''' Converts time-string "HH:MM:SS,SSS" or "HH:MM:SS.SSS"
+    ''' Converts time-string "HH:MM:SS.SSS"
         to seconds (float).
         Returns None, if string doesn't match
     '''
     # Pattern für den Match auf 'nn:nn:nn --> nn:nn:nn'
-    timestampPattern = re.compile(r'(\d+)\:(\d+)\:([\d,\.]+)')
+    timestampPattern = re.compile('(\d+)\:(\d+)\:([\d,\.]+)')
 
     m = timestampPattern.search(line)
     if m:
-        #print(m.groups())      # Angabe der Match-Inhalte aller Gruppen als Tupel
-        #print(m.group(0))      # bzw.: m.group() ... Alles von erster bis letzter Gruppe
-        #print(m.group(1))      # Angabe des Match-Inhalts der 1. Gruppe als String, usw
-        #print(m.group(2))
-        
+       
         hhStr = m.group(1)
         mmStr = m.group(2)
         ssStr = m.group(3)
@@ -49,7 +49,6 @@ def hhmmsssssLineToSecondsFloat(line):
 
 def hhmmsssssToSecondsFloat(hhStr, mmStr, ssStr):
     ''' Converts time-values h, m, s to seconds (float)
-        Returns timeFloat (float)
     '''
     hhInt = int(hhStr)
     mmInt = int(mmStr)
@@ -59,7 +58,6 @@ def hhmmsssssToSecondsFloat(hhStr, mmStr, ssStr):
 
 def secondsFloatToHhmmsssss(timeFloat):
     ''' Converts seconds (float) to time-string "HH:MM:SS.SSS"
-        Returns timeString
     '''
     mm, ss = divmod(timeFloat, 60)
     hh, mm = divmod(mm, 60)
@@ -101,27 +99,51 @@ for line in iniLinesList: # Ausgabe Textdatei
         # Store the values in variables
         if key == "inputFileName" :
             inputFileName = value
-
-        elif key == "addSeconds" :
-            addSecondsFloat = float(value)
             
-print ("inputFileName=" + inputFileName)  # Test
-print ("addSecondsFloat=" + str(addSecondsFloat))  # Test
+        elif key == "time1oldInSRT" :
+            t1OldStr = value
+            
+        elif key == "time1inVideo" :
+            t1NewStr = value
+            
+        elif key == "time2oldInSRT" :
+            t2OldStr = value
+            
+        elif key == "time2inVideo" :
+            t2NewStr = value
+  
+#print ("inputFileName=" + inputFileName)  # Test
+#print ("time1oldInSRT=" + t1OldStr)  # Test
+#print ("time1inVideo=" + t1NewStr)   # Test
+#print ("time2oldInSRT=" + t2OldStr)  # Test
+#print ("time2inVideo=" + t2NewStr)   # Test
 
 # Read the SRT file into linesList
 with open(inputFileName, "r", encoding="utf-8-sig") as f:
     linesList = f.readlines()
 
-# Pattern für den Match auf 'nn:nn:nn --> nn:nn:nn'
-timestampPattern = re.compile(r'(\d+)\:(\d+)\:([\d,\.]+) --> (\d+)\:(\d+)\:([\d,\.]+)')
+# Calculate the time values
+t1Old = hhmmsssssLineToSecondsFloat(t1OldStr)
+t1New = hhmmsssssLineToSecondsFloat(t1NewStr)
+t2Old = hhmmsssssLineToSecondsFloat(t2OldStr)
+t2New = hhmmsssssLineToSecondsFloat(t2NewStr)
+deltaOld = t2Old - t1Old
+deltaNew = t2New - t1New
+coeff = deltaNew/deltaOld    # this is the speed coefficient
+bias = t1New - t1Old * coeff # this is the bias
+        # So the time calculation is: posNew = posOld * coeff + bias
 
-# Process all lines, write zhe list "outDataArray"
+# Regex-pattern for 'nn:nn:nn --> nn:nn:nn'
+timestampPattern = re.compile('(\d+)\:(\d+)\:([\d,\.]+) --> (\d+)\:(\d+)\:([\d,\.]+)')
+
+# Process all lines and write output
 textOutput = ""
 outDataList = list()
+
 outLineCounter = 0
 for line in linesList:
     line = line.strip()
-    # Is there a match on 'nn:nn:nn --> nn:nn:nn'? Then calculate new times!
+    # If there is a match on 'nn:nn:nn --> nn:nn:nn', calculate new times
     m = timestampPattern.search(line)
     if m:
         
@@ -133,45 +155,43 @@ for line in linesList:
         hoursEndStr = m.group(4)
         minutsEndStr = m.group(5)
         secondsEndStr = m.group(6)
-        secondsEndStr = secondsEndStr.replace(',', '.') 
+        secondsEndStr = secondsEndStr.replace(',', '.')
         
         startTime = hhmmsssssToSecondsFloat(hoursStartStr, minutsStartStr, secondsStartStr)
         endTime = hhmmsssssToSecondsFloat(hoursEndStr, minutsEndStr, secondsEndStr)
 
-        # Empty the textOutput for next subtitle text
+        # Old text is obsolete
         textOutput = "";
         
-        # Calculate the new times
-        startTimeOutput = startTime + addSecondsFloat
-        if startTimeOutput < 0 :
-            startTimeOutput = 0
-        endTimeOutput = endTime + addSecondsFloat
-        if endTimeOutput < 0 :
-            endTimeOutput = 0
-            
-        # Add the line to outDataList
+        # Calculate new times: posNew = posOld * coeff + bias
+        startTimeOutput = startTime * coeff + bias
+        endTimeOutput = endTime * coeff + bias
+        
+        # Calculate new line, append to outDataList
         startTimeOutputStr = secondsFloatToHhmmsssss(startTimeOutput)
         endTimeOutputStr = secondsFloatToHhmmsssss(endTimeOutput)
         outputLine = startTimeOutputStr + " --> " + endTimeOutputStr
-        outputLine = outputLine.replace('.', ',')  # In SRT files number format is 1,23     
-        print(outputLine)  # Test
+        outputLine = outputLine.replace('.', ',')   # In SRT files number format is 1,23    
+        #print(outputLine)  # Test
         
         outDataList.append(outputLine)
         outDataList.append("\n")
-        
+
     else :
-        # print("Test: This is an empty line");   # Test
+        # print("Empty line");   # Test
         outLineCounter += 1
         
-        # Add that line to the output too
+        # Append the empty line
         outDataList.append(line)
         outDataList.append("\n")
 
-# Write the output to destination file
+# Store the data in a new SRT file
 outFilename = inputFileName + "_new.srt"
-with open(outFilename, 'w', encoding="utf-8") as f :
-    for item in outDataList :
+with open(outFilename, 'w', encoding="utf-8") as f:
+    for item in outDataList:   # Write all lines into file
         f.write(item)
 
 print("Job done: " + outFilename)
+
+
 
